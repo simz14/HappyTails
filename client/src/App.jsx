11276d494b4de73153dd4af5f8a40b635f79da23
Simona Zozuľaková <szozulakova@gmail.com>
2023-03-27 21:41:59 +0200

    creating folder structure, installing packages

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Routes } from "react-router";
+import Home from "./screens/Home/Home";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+};
+
+export default App;
