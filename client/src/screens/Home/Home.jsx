aaaeef7bc6f6fb4a3fac1bb8a6c4bab4ce407c1e
Simona Zozuľaková <szozulakova@gmail.com>
2023-04-14 12:36:40 +0200

    meta image

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -7,7 +7,6 @@ import ClientsSection from "./sections/ClientsSection";
 import LearnMoreSection from "./sections/LearnMore";
 import ScrollTop from "../../components/ScrollTop";
 import { Helmet } from "react-helmet-async";
-import DogsGroup from "../../assets/DogsGroup.jpg";
 
 const Home = () => {
   return (
@@ -16,7 +15,10 @@ const Home = () => {
         <html lang="en" />
         <title>HappyTails</title>
         <meta property="og:title" content="HappyTails Dog Shelter" />
-        <meta property="og:image" content={DogsGroup} />
+        <meta
+          property="og:image"
+          content="https://img.freepik.com/free-photo/group-portrait-adorable-puppies_53876-64778.jpg?w=1380&t=st=1681468573~exp=1681469173~hmac=f7281b58b0163b5ed3b58cfc438a3bc6453a6c7c691aa87f80486b5b8aa46d2a"
+        />
         <meta
           property="og:description"
           content="Our dog shelter provides a safe and loving environment for dogs who have been abandoned or surrendered. We provide medical care, socialization, and training to help prepare our dogs for their forever homes. Our ultimate goal is to find each dog a loving and permanent family to call their own."
